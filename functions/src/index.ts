a13b298ab19069d6df17c349e44fae0371fecae0
tiwu <theimaginationwasunbroken@gmail.com>
2019-08-15 22:18:26 +0900

    firebase hosting, functions, express init

---
+++ functions/src/index.ts
@@ -0,0 +1,29 @@
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;
+    res.send(`
+    <!doctype html>
+    <head>
+      <title>Time</title>
+      <link rel="stylesheet" href="/style.css">
+      <script src="/script.js"></script>
+    </head>
+    <body>
+      <p>In London, the clock strikes:
+        <span id="bongs">${'BONG '.repeat(hours)}</span></p>
+      <button onClick="refresh(this)">Refresh</button>
+    </body>
+  </html>`);
+});
+
+app.get('/api', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.json({bongs: 'BONG '.repeat(hours)});
+});
+
+exports.app = functions.https.onRequest(app);
