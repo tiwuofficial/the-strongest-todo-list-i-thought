7a9565dede29fa06741ec0e0f54d9813136364f2
tiwu <theimaginationwasunbroken@gmail.com>
2019-08-16 15:22:00 +0900

    use ejs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,22 +2,22 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    const date = new Date();
-    const hours = (date.getHours() % 12) + 1;
-    res.send(`
-    <!doctype html>
-    <head>
-      <title>Time</title>
-      <link rel="stylesheet" href="/style.css">
-      <script src="/script.js"></script>
-    </head>
-    <body>
-      <p>In London, the clock strikes:
-        <span id="bongs">${'BONG '.repeat(hours)}</span></p>
-      <button onClick="refresh(this)">Refresh</button>
-    </body>
-  </html>`);
+    res.status(200).render("index",
+        {
+            items: [
+                {name: "<h1>リンゴ</h1>"},
+                {name: "<h2>バナナ</h2>"},
+                {name: "<h3>スイカ</h3>"}
+            ]
+        }
+    );
+});
+
+app.get('/hoge', (req, res) => {
+    res.status(200).render('hoge');
 });
 
 app.get('/api', (req, res) => {
